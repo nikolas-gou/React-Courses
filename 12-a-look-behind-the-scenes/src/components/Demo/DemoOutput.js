a43d9f775747396084a4681d6f005006e7cf514b
nikolas-gou <nikolasgou23@gmail.com>
2023-01-14 01:16:56 +0200

    Update Closer look at child component re-evaluation

--- 12-a-look-behind-the-scenes/src/components/Demo/DemoOutput.js
+++ 12-a-look-behind-the-scenes/src/components/Demo/DemoOutput.js
@@ -1,8 +1,9 @@
 import React from "react";
+import MyParagraph from "./MyParagraph";
 
 const DemoOutput = (props) => {
   console.log("DemoOutput RUNNING");
-  return <p>{props.show ? 'This is new!' : ''}</p>;
+  return <MyParagraph>{props.show ? 'This is new!' : ''}</MyParagraph>;
 };
 
 export default DemoOutput;
