7024331f38cb6c0f78b9a46623e1c43c532a5b16
nikolas-gou <nikolasgou23@gmail.com>
2023-01-15 21:41:08 +0200

    First commit

---
+++ 04-react-state-events/src/components/NewExpense/NewExpense.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import ExpenseForm from './ExpenseForm';
+import './NewExpense.css';
+
+const NewExpense = () => {
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString()
+    };
+    console.log(expenseData);
+  };
+
+  return (
+    <div className='new-expense'>
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+    </div>
+  );
+};
+
+export default NewExpense;
