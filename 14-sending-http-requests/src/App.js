4cc7caab2370d06e547b0fc9d2a9773686d609ba
nikolas-gou <nikolasgou23@gmail.com>
2023-02-01 03:20:52 +0200

    Transforming format of API object

--- 14-sending-http-requests/src/App.js
+++ 14-sending-http-requests/src/App.js
@@ -12,8 +12,16 @@ function App() {
         return response.json();
       })
       .then((data) => {
-        setMovies(data.results);
-      })
+        const transformedMovies = data.results.map((movieData) => {
+          return {
+            id: movieData.episode_id,
+            title: movieData.title,
+            openingText: movieData.opening_crawl,
+            releaseDate: movieData.release_date
+          };
+        })
+        setMovies(transformedMovies);
+      });
   }
 
   return (
