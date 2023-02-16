d12bebe49464420b735d8584dbe6ab61eb4ba3b1
nikolas-gou <nikolasgou23@gmail.com>
2023-02-16 02:10:29 +0200

    Adding useRef Handler

--- 16-working-with-forms/src/components/SimpleInput.js
+++ 16-working-with-forms/src/components/SimpleInput.js
@@ -2,6 +2,7 @@ import { useRef, useState } from 'react';
 
 const SimpleInput = (props) => {
   const [enteredName, setEnteredName] = useState('');
+  const nameInputRef = useRef();
 
   const nameInputHandler = (event) => {
     setEnteredName(event.target.value);
@@ -11,13 +12,24 @@ const SimpleInput = (props) => {
     event.preventDefault();
 
     console.log(enteredName);
+
+    const enteredValue = nameInputRef.current.value;
+    console.log(enteredValue);
+    
+    setEnteredName('');
   }
 
   return (
     <form onSubmit={formSubmissionHandler}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' onChange={nameInputHandler} />
+        <input 
+          ref={nameInputRef} 
+          type='text' 
+          id='name' 
+          onChange={nameInputHandler} 
+          value={enteredName}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
