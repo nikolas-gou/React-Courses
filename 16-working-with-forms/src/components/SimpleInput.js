d6c43f09e2b8e90e17eada3e2896074b863f75fd
nikolas-gou <nikolasgou23@gmail.com>
2023-02-16 02:00:15 +0200

    Adding state handler

--- 16-working-with-forms/src/components/SimpleInput.js
+++ 16-working-with-forms/src/components/SimpleInput.js
@@ -1,9 +1,23 @@
+import { useRef, useState } from 'react';
+
 const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState('');
+
+  const nameInputHandler = (event) => {
+    setEnteredName(event.target.value);
+  }
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+
+    console.log(enteredName);
+  }
+
   return (
-    <form>
+    <form onSubmit={formSubmissionHandler}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input type='text' id='name' onChange={nameInputHandler} />
       </div>
       <div className="form-actions">
         <button>Submit</button>
