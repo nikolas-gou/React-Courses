adfe68fe731c5c1c4470c5ab4439bd80a17be890
nikolas-gou <nikolasgou23@gmail.com>
2023-02-18 18:14:30 +0200

    Handling the loading state

--- 17-practice-food-order-http-forms/src/components/Meals/AvailableMeals.js
+++ 17-practice-food-order-http-forms/src/components/Meals/AvailableMeals.js
@@ -1,10 +1,13 @@
+import { useEffect, useState } from 'react';
+
 import Card from '../UI/Card';
 import MealItem from './MealItem/MealItem';
 import classes from './AvailableMeals.module.css';
-import { useEffect, useState } from 'react';
+
 
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchMeals = async () => {
@@ -21,10 +24,16 @@ const AvailableMeals = () => {
         });
       }
       setMeals(loadedMeals);
+      setIsLoading(false);
     };
-
     fetchMeals();
   }, []);
+
+  if(isLoading) {
+    return <section className={classes.MealsLoading}>
+      <p>Loading...</p>
+    </section>
+  }
 
   const mealsList = meals.map((meal) => (
     <MealItem
