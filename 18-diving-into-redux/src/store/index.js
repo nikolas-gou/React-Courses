2e1dacae2c33cc6c51904c79e12ca3770cdbcff7
nikolas-gou <nikolasgou23@gmail.com>
2023-02-19 20:50:32 +0200

    Adding state slices

--- 18-diving-into-redux/src/store/index.js
+++ 18-diving-into-redux/src/store/index.js
@@ -1,35 +1,59 @@
 import { createStore } from 'redux';
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   counter: 0,
   showCounter: true,
 };
 
+createSlice({
+  name: 'counter',
+  initialState: initialState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter += action.amount;
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter;
+    },
+  }
+});
+
 const counterReducer = (state = initialState, action) => {
   if(action.type === 'increment') {
     return {
+      ...state,
       counter: state.counter + 1,
-      showCounter: state.showCounter,
+      // showCounter: state.showCounter,
     };
   }
 
   if(action.type === 'increase') {
     return {
+      ...state,
       counter: state.counter + action.amount,
-      showCounter: state.showCounter,
+      // showCounter: state.showCounter,
     };
   }
 
   if(action.type === 'decrement') {
     return {
+      ...state,
       counter: state.counter - 1,
-      showCounter: state.showCounter,
+      // showCounter: state.showCounter,
     };
   }
 
   if(action.type === 'toggle') {
     return {
-      counter: state.counter,
+      ...state,
+      // counter: state.counter,
       showCounter: !state.showCounter,
     };
   }
