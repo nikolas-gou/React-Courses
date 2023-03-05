917f6a3065b100f6349328b55baa213007e0a1f3
nikolas-gou <nikolasgou23@gmail.com>
2023-03-05 14:55:08 +0200

    Defining routes

--- 20-building-a-multi-page-spa-with-react-router/src/App.js
+++ 20-building-a-multi-page-spa-with-react-router/src/App.js
@@ -1,9 +1,12 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import HomePage from './pages/Home';
+
+const router = createBrowserRouter([
+  { path: '/', element: <HomePage />}
+]);
+
 function App() {
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
-  );
+  return <RouterProvider router={router}/>;
 }
 
 export default App;
---
+++ 20-building-a-multi-page-spa-with-react-router/src/pages/Home.js
@@ -0,0 +1,5 @@
+function HomePage() {
+  return <h1>My home page</h1>;
+}
+
+export default HomePage;
