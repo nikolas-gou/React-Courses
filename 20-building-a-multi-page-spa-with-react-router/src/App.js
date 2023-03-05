116ee55d093e9a62d7ca0d2c52a34740208ca99e
nikolas-gou <nikolasgou23@gmail.com>
2023-03-05 15:16:19 +0200

    Navigating into pages via Link

--- 20-building-a-multi-page-spa-with-react-router/src/App.js
+++ 20-building-a-multi-page-spa-with-react-router/src/App.js
@@ -2,18 +2,11 @@ import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } 
 import HomePage from './pages/Home';
 import ProductsPage from './pages/Products';
 
-const routeDefinitions = createRoutesFromElements(
-  <Route>
-    <Route path="/" element={<HomePage />} />
-  </Route>
-);
+const router = createBrowserRouter([
+  { path: '/', element: <HomePage />},
+  { path: '/products', element: <ProductsPage />}
+]);
 
-// const router = createBrowserRouter([
-//   { path: '/', element: <HomePage />},
-//   { path: '/products', element: <ProductsPage />}
-// ]);
-
-const router = createBrowserRouter(routeDefinitions);
 
 function App() {
   return <RouterProvider router={router}/>;
--- 20-building-a-multi-page-spa-with-react-router/src/pages/Home.js
+++ 20-building-a-multi-page-spa-with-react-router/src/pages/Home.js
@@ -1,5 +1,14 @@
+import { Link } from "react-router-dom";
+
 function HomePage() {
-  return <h1>My home page</h1>;
-}
+  return (
+  <>
+    <h1>My home page</h1>;
+    <p>
+      Go to <Link to="/products">The list of products</Link>
+    </p>
+  </>
+  );
+};
 
 export default HomePage;
