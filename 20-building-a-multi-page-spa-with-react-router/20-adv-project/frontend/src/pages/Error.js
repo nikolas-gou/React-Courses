03eb0da989f2db1d64a31c42eb00247a332f3a1d
nikolas-gou <nikolasgou23@gmail.com>
2023-04-18 14:47:44 +0300

    json utility function

--- 20-building-a-multi-page-spa-with-react-router/20-adv-project/frontend/src/pages/Error.js
+++ 20-building-a-multi-page-spa-with-react-router/20-adv-project/frontend/src/pages/Error.js
@@ -8,7 +8,7 @@ function ErrorPage() {
   let message = 'Something wrong!';
 
   if(error.status === 500) {
-    message = JSON.parse(error.data).message;
+    message = error.data.message;
   }
 
   if(error.status === 404) {
--- 20-building-a-multi-page-spa-with-react-router/20-adv-project/frontend/src/pages/Events.js
+++ 20-building-a-multi-page-spa-with-react-router/20-adv-project/frontend/src/pages/Events.js
@@ -1,35 +1,25 @@
-import { useEffect, useState } from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData, json } from "react-router-dom";
 
-import EventsList from '../components/EventsList';
+import EventsList from "../components/EventsList";
 
 function EventsPage() {
-  const data = useLoaderData();
-  // if(data.isError) {
-  //   return <p>{data.message}</p>
-  // }
-  const events = data.events;
+    const data = useLoaderData();
+    const events = data.events;
 
-  return (
-    <>
-      <EventsList events={events} />
-    </>
-  );
+    return (
+        <>
+            <EventsList events={events} />
+        </>
+    );
 }
 
 export default EventsPage;
 
 export async function loader() {
-  const response = await fetch('http://localhost:8080/events');
-  if (!response.ok) {
-    // return { isError: true, message: "Could not fetch events."};
-    throw new Response(JSON.stringify({ message: 'Could not fetch events.'}), { 
-      status: 500
-    });
-  } else {
-    //const resData = await response.json();
-    //return resData.events;
-    //const res  = new Response("any data", {status: 201});
-    return response;
-  }
-} 
+    const response = await fetch("http://localhost:8080/events");
+    if (!response.ok) {
+        throw json({ message: "Could not fetch events." }, { status: 500 });
+    } else {
+        return response;
+    }
+}
